4f8f3c5ed577aa01f2dc1c160e6fa73a3db72332
Frank McDonough <frankiem@fm4.net>
2025-12-17 15:38:12 -0500

    updated config so github pages will work

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import { defineConfig } from 'astro/config';
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-    site: 'https://cscie12.github.io',
-  base: isProduction ? '/final-project-frankiem-4' : '/',
+  site: 'https://frankiem-4.github.io',
+  base: isProduction ? '/drone-strike-explorer' : '/',
 });
