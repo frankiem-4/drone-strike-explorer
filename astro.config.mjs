5a8434932c24a846683755ed53940e9cbd6860c4
Frank McDonough <frankiem@fm4.net>
2025-12-17 15:50:09 -0500

    another fix for the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   site: 'https://frankiem-4.github.io',
-  base: isProduction ? '/drone-strike-explorer' : '/',
+  base: isProduction ? '/drone-strike-explorer/' : '/',
 });
